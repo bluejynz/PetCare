261bf3a9d2cc1349d22d04eb8f32d87e69795b9e
bluejynz <danifalk27@gmail.com>
2021-06-21 19:52:08 -0300

    final

--- src/ContainerHome/Index.jsx
+++ src/ContainerHome/Index.jsx
@@ -1,9 +1,9 @@
+import './style.css'
+
 const ContainerHome = () => {
     return <div className="container">
-        <div className="children">
-            <h1>PetCare</h1>
-            <img src="https://i.ytimg.com/vi/C3xO4QpC53s/maxresdefault.jpg" alt="Pets" />
-        </div>
+        <img src="https://i.ytimg.com/vi/C3xO4QpC53s/maxresdefault.jpg" alt="" />
+        <h1>PetCare</h1>
     </div>
 }
 
